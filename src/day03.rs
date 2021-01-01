c3ac3e55ff5b9690191335d858ac7dcb30987c05
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-01-01 15:48:49 +0100

    day 3 part 1

---
+++ src/day03.rs
@@ -0,0 +1,105 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::{BufRead, BufReader, Seek};
+use std::io;
+use std::error::Error;
+
+pub fn run(input_path: &Path) -> bool {
+    let map = match parse(input_path) {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("There was a problem parsing the input file:");
+            eprintln!("{}", e);
+            return false;
+        }
+    };
+
+    println!(
+        "Num trees along direction (3, 1): {}",
+        count_trees_along_direction(&map, 3,1)
+    );
+
+    return true;
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+enum TileType {
+    Open,
+    Tree
+}
+
+#[derive(Debug)]
+struct TobogganMap {
+    rows: usize,
+    cols: usize,
+    tiles: Vec<TileType>
+}
+impl TobogganMap {
+    fn get_tile(&self, x: usize, y: usize) -> TileType {
+        let _x = x % self.cols;
+        let _y = y;
+
+        self.tiles[ self.cols * _y + _x]
+    }
+}
+
+fn parse(filepath: &Path) -> Result<TobogganMap, Box<dyn Error>> {
+    let mut file = File::open(filepath).expect("Could not open file");
+    let num_lines = BufReader::new(&file).lines().count();
+    file.seek(io::SeekFrom::Start(0))?;
+
+    let rows = num_lines;
+    let cols;
+    let mut tiles: Vec<TileType>;
+    {
+        
+        let line = BufReader::new(&file).lines().nth(0).unwrap()?;
+        cols = line.chars().count();
+        tiles = vec![TileType::Open; rows*cols];
+        file.seek(io::SeekFrom::Start(0))?;
+    }
+
+    let reader = BufReader::new(&file);
+    for (i_line, line) in reader.lines().enumerate() {
+        for (i_c, c) in line?.chars().enumerate() {
+            let tile_type = match c {
+                '.' => Ok(TileType::Open),
+                '#' => Ok(TileType::Tree),
+                _ => Err(
+                    Box::new(
+                        io::Error::new( 
+                            io::ErrorKind::Other, 
+                            format!("Could not parse row {} col {}: unexpected charachter {}", i_line, i_c, c)
+                        )
+                    )
+                )
+            }?;
+
+            tiles[i_line * cols + i_c] = tile_type;
+        }
+    }
+
+    Ok(TobogganMap {
+        rows,
+        cols,
+        tiles
+    })
+
+}
+
+fn count_trees_along_direction(map: &TobogganMap, dx: usize, dy: usize) -> usize { 
+    let mut x: usize = 0;
+    let mut y: usize = 0;
+    let mut num_trees = 0;
+
+    while y + dy <= map.rows {
+        if map.get_tile(x, y) == TileType::Tree {
+            num_trees += 1;
+        }
+
+        x += dx;
+        y += dy;
+    }
+
+    num_trees
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 
 mod day01;
 mod day02;
+mod day03;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,6 +12,7 @@ fn main() {
     let success = match day {
         Some("01") => day01::run(&Path::new("input/day01.txt")),
         Some("02") => day02::run(&Path::new("input/day02.txt")),
+        Some("03") => day03::run(&Path::new("input/day03.txt")),
         _ => { eprintln!("day not found"); false }
     };
     
