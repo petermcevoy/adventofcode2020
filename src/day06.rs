4e642fe19eee1877206b3ed4d779012ff08fe74a
Peter McEvoy <peter.michael.mcevoy@gmail.com>
2021-02-15 21:46:05 +0100

    Day 6 part 2

--- src/day06.rs
+++ src/day06.rs
@@ -4,19 +4,27 @@ use std::io::{BufRead, BufReader};
 use std::collections::HashSet;
 
 pub fn run(input_path: &Path) -> bool {
+    
+    println!("Part1");
     let file = File::open(input_path).expect("Could not open file");
-    let reader = BufReader::new(&file);
-    let declarations = parse(reader);
-
-    let sum: usize = declarations.iter().map(|decl| decl.len()).sum();
+    let declarations_part1 = parse_part1(BufReader::new(&file));
+    let sum: usize = declarations_part1.iter().map(|decl| decl.len()).sum();
     println!("Sum: {}", sum);
+    assert_eq!(sum, 6443);
+    
+    println!("Part2");
+    let file = File::open(input_path).expect("Could not open file");
+    let v = parse_part2(BufReader::new(&file));
+    let sum: u32 = v.iter().map(|decl| decl).sum();
+    println!("Sum: {}", sum);
+    assert_eq!(sum, 3232);
 
     true
 }
 
 type GroupDeclaration = HashSet<char>;
 
-fn parse<R: BufRead>(reader: R) -> Vec<GroupDeclaration> {
+fn parse_part1<R: BufRead>(reader: R) -> Vec<GroupDeclaration> {
     let mut declarations: Vec<GroupDeclaration> = Vec::new();
     let mut current_declaration = GroupDeclaration::new();
 
@@ -38,13 +46,50 @@ fn parse<R: BufRead>(reader: R) -> Vec<GroupDeclaration> {
     declarations
 }
 
+fn parse_part2<R: BufRead>(reader: R) -> Vec<u32> {
+    let mut declarations: Vec<u32> = Vec::new();
+
+    let input_vec: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
+    let input = input_vec.join("\n");
+    
+    for group_decl_str in input.split("\n\n") {
+        
+        // Within the group, how many questions did everyone answer yes to?
+        // answers are from a-z. We need 26 positions -> 32 bits will do.
+        let mut group_bitflags: u32 = 0xffffffff;
+
+        for individual_decl_str in group_decl_str.lines() {
+            // For the individual within the group, make a mask of all
+            // the questions they answered yes to.
+            let mut individual_mask: u32 = 0x0;
+            for c in individual_decl_str.chars() {
+                if ('a'..='z').contains(&c) {
+                    let mut b = [0; 1];
+                    c.encode_utf8(&mut b);
+                    let asci_count: u8 = b[0] - b'a';
+                    let bitflag: u32 = 0b1 << asci_count;
+                    individual_mask = individual_mask | bitflag;
+                } else {
+                    panic!("Unexpected answer, should be a..z!");
+                }
+
+            }
+            
+            group_bitflags = group_bitflags & individual_mask;
+        }
+        let num_yes = group_bitflags.count_ones();
+        declarations.push(num_yes);
+        //println!("---\n{}\ncount: {}", group_decl_str, num_yes);
+    }
+
+    declarations
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn example() {
-        let example_input = "\
+    const EXAMPLE_INPUT: &str = "\
 abc
 
 a
@@ -60,7 +105,10 @@ a
 a
 
 b";
-        let declarations = parse(BufReader::new(example_input.as_bytes()));
+
+    #[test]
+    fn example_part1() {
+        let declarations = parse_part1(BufReader::new(EXAMPLE_INPUT.as_bytes()));
         for (i, dec) in declarations.iter().enumerate() {
             println!("{}: {}", i, dec.len());
             match i {
@@ -87,6 +135,32 @@ b";
 
         let sum: usize = declarations.iter().map(|decl| decl.len()).sum();
         assert_eq!(11, sum);
+    }
+    
+    #[test]
+    fn example_part2() {
+        let declarations = parse_part2(BufReader::new(EXAMPLE_INPUT.as_bytes()));
+        for (i, dec) in declarations.iter().enumerate() {
+            println!("{}: {}", i, dec);
+            match i {
+                0 => {
+                    assert_eq!(dec, &3);
+                },
+                1 => {
+                    assert_eq!(dec, &0);
+                },
+                2 | 3 => {
+                    assert_eq!(dec, &1);
+                },
+                4 => {
+                    assert_eq!(dec, &1);
+                },
+                _ => { panic!("Unexpected group"); }
+            }
+        }
+
+        let sum: u32 = declarations.iter().map(|decl| decl).sum();
+        assert_eq!(7, sum);
     }
 }
 
